06f7fdd8b3af55d9b410d129beb525cfcc027bd6
Marcos Zaghetto <jr.zaghetto@gmail.com>
2022-05-12 21:11:11 -0300

    passou geral

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const users = this.usersRepository.list();
+
+    const user = users.find((user) => user.id === user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("User is not admin");
+    }
+
+    return users;
   }
 }
 
